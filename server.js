d8e2e36852f38de9420501c0080c985fca41a322
hermsicles <hermanliu95@gmail.com>
2021-09-15 15:40:31 -0700

    remove .env

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const mongoose = require("mongoose");
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/crud";
+const MONGO_URI = "mongodb://localhost/crud";
 const MONGO_OPTIONS = {};
 const path = require("./build");
 
@@ -19,11 +19,6 @@ connection();
 //Set up middleware otherwise can't connect. Data Parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "build", "index.html"))
-);
 
 const apiRoutes = require("./routes/apiRoutes");
 app.use("/api", apiRoutes);
