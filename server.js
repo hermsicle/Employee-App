4589066df28d3555331a74aacd39f32a4943429e
hermsicles <hermanliu95@gmail.com>
2021-09-15 19:02:23 -0700

    changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/crud";
 const MONGO_OPTIONS = {};
